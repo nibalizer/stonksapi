3cb53766a86e4b7962352262716967e2d64d1e5c
Spencer Krum <nibz@spencerkrum.com>
2020-08-30 18:43:41 +0000

    Entirely move auth out of api

--- v1/stonks.go
+++ v1/stonks.go
@@ -48,12 +48,14 @@ func GetDailyChange(quote finnhub.Quote) (percent float32) {
 	return percent
 }
 
-func Quote(symbol string, preRona bool, records [][]string) (detail QuoteDetail, err error) {
-	finnhubClient := finnhub.NewAPIClient(finnhub.NewConfiguration()).DefaultApi
-	auth := context.WithValue(context.Background(), finnhub.ContextAPIKey, finnhub.APIKey{
-		Key: os.Getenv("FINNHUB_API_KEY"),
-	})
+func Quote(symbol string, preRona bool, records [][]string, finnhubClient *finnhub.DefaultApiService, auth context.Context) (detail QuoteDetail, err error) {
+	/*
+		finnhubClient := finnhub.NewAPIClient(finnhub.NewConfiguration()).DefaultApi
+		auth := context.WithValue(context.Background(), finnhub.ContextAPIKey, finnhub.APIKey{
+			Key: os.Getenv("FINNHUB_API_KEY"),
+		})
 
+	*/
 	log.Printf("Looking up stock quote: %s\n", symbol)
 	quote, _, err := finnhubClient.Quote(auth, symbol)
 	if err != nil {
