b1827d8b15011fd63dc6a4e2c21feed74f8bc667
Spencer Krum <nibz@spencerkrum.com>
2021-01-18 18:20:19 -0600

    Add stonksdata scripts

---
+++ contrib/get_stonks_db.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+#
+#
+#
+#
+#159 >---// stonksdata.txt from:
+#160 >---// ftp://ftp.nasdaqtrader.com/SymbolDirectory/
+#161 >---// cat nasdaqlisted.txt otherlisted.txt mfundslist.txt |cut -d "|" -f 1-3 > stonksdata.txt
+
+echo "running"
+date
+rm -f nasdaqlisted.txt otherlisted.txt mfundslist.txt  stonksdata.txt
+
+echo "ftp commands to run:"
+cat ftpcmds.txt
+
+echo ""
+echo ""
+
+echo "sending ftp commands"
+#ftp -v -n < ftpcmds.txt
+lftp -v -f ftpcmds.txt
+cat nasdaqlisted.txt otherlisted.txt mfundslist.txt |cut -d "|" -f 1-3 > stonksdata.txt
+
+echo "Created db: stonksdata.txt"
+
+
+
